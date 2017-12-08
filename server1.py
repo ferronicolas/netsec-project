3073d0048477554fb6b93fe3b452f0099b75a3a4
Sam Wagenvoorde <Sam@MacBook-Pro-van-Sam.local>
2017-12-08 09:28:59 -0500

    Cleaned up code

--- server1.py
+++ server1.py
@@ -14,6 +14,7 @@ LIST_RESPONSE_MESSAGE = "list_response"
 GET_ADDRESS_OF_USER = "get_address_of_user"
 GET_ADDRESS_OF_USER_RESPONSE = "get_address_of_user_response"
 ILLEGAL_MESSAGE_RESPONSE = "illegal"
+PUZZLE_MESSAGE = 'puzzle'
 
 # Error messages
 ERROR_MUST_ENTER_POSITIVE = "You must enter a positive number"
@@ -29,8 +30,8 @@ server_socket = None
 pow_length = 3
 
 # Hardcoded values
-#server_private_key = "path/to/private/key"
-#server_public_key = "path/to/public/key"
+server_private_key = "path/to/private/key"
+server_public_key = "path/to/public/key"
 
 #threads
 threads = []
@@ -87,31 +88,23 @@ def listen_on_port(port):
             print message
             if len(message) > 0:
                 if message[0] == SIGN_IN_MESSAGE:
-                    current_users[message[1]] = (address[0], address[1])  # USERNAME: (IP address, port)
+                    #current_users[message[1]] = (address[0], address[1])  # USERNAME: (IP address, port)
                     global pow_length
                     pow_length = incoming_control.get_pow_length(pow_length)
                     r1,r2,hash_value = PoW.proof_o_work(pow_length)
-                    print r2
+                    #store r2
+                    #add puzzle identifier?
                     server_socket.sendto(str(r1) + ',' + str(hash_value), address)
-                    print 'finished'
-                    #send r2 to client
-                    #data, address = server_socket.recvfrom(BUFFER_SIZE)
                 elif message[0] == LIST_MESSAGE:
                     server_socket.sendto(make_current_users_message(), address)
                 elif message[0] == GET_ADDRESS_OF_USER:
                     server_socket.sendto(make_get_address_of_user_response(message[1]), address)
-                elif message[0] == 'puzzle':
-                    print PoW.check_proof_o_work(r1, message[1],hash_value)
+                elif message[0] == PUZZLE_MESSAGE:
                     if PoW.check_proof_o_work(r1, message[1],hash_value):
                         username, password, r1, df_contribution = message[2].split(',')
-                        print df_contribution
                         #check password
                         shared_key, server_pubkey = Diffie_hellman.server_contribution(int(df_contribution))
                         server_socket.sendto(str(r1) + ',' + str(server_pubkey), address)
-                        print 'shared_key:', shared_key
-
-                    else:
-                        print 'pow failed'
             else:
                 server_socket.sendto(ILLEGAL_MESSAGE_RESPONSE + " You just typed in an invalid command", address)
     except socket.error, exc:  # If address is already in use it will throw this exception
